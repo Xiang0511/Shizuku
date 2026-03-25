337c9b3af13b9cf88c122c2b6dec97d3b648ca20
Sean <sealll40@gmail.com>
2026-03-25 10:19:09 +0800

    feat: 新增List與Block_List方法

--- Shizuku/Controllers/MemberController.cs
+++ Shizuku/Controllers/MemberController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shizuku.Models;
+using Shizuku.ViewModels;
 
 namespace Shizuku.Controllers
 {
@@ -7,6 +9,24 @@ namespace Shizuku.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List(CKeywordViewModel vm)
+        {
+            DbShizukuDemoContext db = new DbShizukuDemoContext();
+            IEnumerable<TMember> datas = null;
+            datas = from p in db.TMembers
+                    select p;
+            return View(datas);
+        }
+        public IActionResult Block_List(CKeywordViewModel vm)
+        {
+
+            DbShizukuDemoContext db = new DbShizukuDemoContext();
+            IEnumerable<TMember> datas = null;
+            datas = from p in db.TMembers
+                    select p;
+            return View(datas);
         }
     }
 }
