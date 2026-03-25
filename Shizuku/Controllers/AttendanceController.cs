7f634b2533aa9949dfc24a833711a0aef3849ce9
wayne <s0921233527@gmail.com>
2026-03-26 00:28:50 +0800

    feat:新增AttendanceController,準備製作打卡系統

---
+++ Shizuku/Controllers/AttendanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shizuku.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
