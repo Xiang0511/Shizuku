d889c4d110ea978cb3b31d91f6cd44e74fe1c94d
AnnZi <sealll40@gmail.com>
2026-03-26 22:10:42 +0800

    feat: 建立SystemController

---
+++ Shizuku/Controllers/SystemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shizuku.Controllers
+{
+    public class SystemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
