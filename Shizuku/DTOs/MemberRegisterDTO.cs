51e43542ab8464baaf30c4da0562f9593801cab3
Sean <sealll40@gmail.com>
2026-05-05 12:46:51 +0800

    feat: 建立註冊的DTO

---
+++ Shizuku/DTOs/MemberRegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Shizuku.DTOs
+{
+    public class MemberRegisterDTO
+    {
+        public string FName { get; set; } = null!;
+        public string FEmail { get; set; } = null!;
+        public string FPassword { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
