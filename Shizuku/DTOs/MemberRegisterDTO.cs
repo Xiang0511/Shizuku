2c49654bb5564364c0b5cdb831002b5060681164
Sean <sealll40@gmail.com>
2026-05-05 16:21:09 +0800

    feat: 更新會員註冊欄位一定不能為空值
    - postman不會傳空值可以註冊了

--- Shizuku/DTOs/MemberRegisterDTO.cs
+++ Shizuku/DTOs/MemberRegisterDTO.cs
@@ -1,10 +1,20 @@
-﻿namespace Shizuku.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+namespace Shizuku.DTOs
 {
     public class MemberRegisterDTO
     {
+        [Required(ErrorMessage = "姓名不能為空")]
         public string FName { get; set; } = null!;
+
+        [Required(ErrorMessage = "電子郵件不能為空")]
+        [EmailAddress(ErrorMessage = "電子郵件格式不正確")]
         public string FEmail { get; set; } = null!;
+
+        [Required(ErrorMessage = "密碼不能為空")]
+        [MinLength(6, ErrorMessage = "密碼長度至少需 6 個字元")]
         public string FPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "確認密碼不能為空")]
         public string ConfirmPassword { get; set; } = null!;
     }
 }
