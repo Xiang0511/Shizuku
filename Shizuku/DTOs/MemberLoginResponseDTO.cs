bc451888c7cae36a7edaad45e680eb8642792668
Sean <sealll40@gmail.com>
2026-05-16 17:05:33 +0800

    feat: 更新登入回傳DTO
    - 回傳帳戶等級
    - 用於顯示帳戶等級
    - 可用於權限區分

--- Shizuku/DTOs/MemberLoginResponseDTO.cs
+++ Shizuku/DTOs/MemberLoginResponseDTO.cs
@@ -12,6 +12,8 @@
 
         public string? FPhone { get; set; }
 
+        public int? FLevel { get; set; }
+
         //[cite_start]// 專業建議：未來若有 JWT Token 或權限，也可加在此處 [cite: 87]
         // public string Token { get; set; }// 新增這一行
         public string? Token { get; set; }
