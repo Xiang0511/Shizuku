2c5725f71d7011f81dc8460e5700d029a0382c33
Sean <sealll40@gmail.com>
2026-05-17 20:00:46 +0800

    feat: 新增會員點數商城Model

--- Shizuku/Models/DbShizukuDemoContext.cs
+++ Shizuku/Models/DbShizukuDemoContext.cs
@@ -59,6 +59,7 @@ public partial class DbShizukuDemoContext : DbContext
     public virtual DbSet<TOrderDetail> TOrderDetails { get; set; }
     public virtual DbSet<TChatbotFaq> TChatbotFaqs { get; set; }
 
+    public virtual DbSet<TMemberStoreItem> TMemberStoreItem { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning OTo protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
@@ -251,6 +252,13 @@ public partial class DbShizukuDemoContext : DbContext
                 .HasColumnType("datetime")
                 .HasColumnName("fUpdated_time");
             entity.Property(e => e.FWishlist).HasColumnName("fWishlist");
+        });
+
+        modelBuilder.Entity<TMemberStoreItem>(entity =>
+        {
+            entity.ToTable("tMemberStoreItem"); // 完美對應你的堅持！
+            entity.HasKey(e => e.FId);
+            entity.HasIndex(e => e.FItemId).IsUnique();
         });
 
         modelBuilder.Entity<TMemberVerification>(entity =>
