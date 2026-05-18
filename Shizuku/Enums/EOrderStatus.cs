562eac707cd9b9bd8b427b78fdaf5ba33eed83ce
Wayne <s0921233527@gmail.com>
2026-05-18 11:55:29 +0800

    feat: 新增 狀態管理 Enums
    - 新增EOrderStatus
    - 新增EPaymentMethod
    - 新增EPaymentStatus

---
+++ Shizuku/Enums/EOrderStatus.cs
@@ -0,0 +1,11 @@
+namespace Shizuku.Enums
+{
+    public enum OrderStatus
+    {
+        Pending = 1,   // 未付款
+        Paid = 2,      // 已付款 (準備出貨)
+        Shipping = 3,  // 出貨中
+        Delivered = 4, // 已送達
+        Cancelled = 5  // 已取消
+    }
+}
